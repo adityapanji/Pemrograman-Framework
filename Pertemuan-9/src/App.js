bb6829ad95989a83888822856fbe8345306bf29b
Aditya Panji Rachmadan <58581060+adityapanji@users.noreply.github.com>
2021-04-12 13:24:41 +0700

    Delete Pertemuan-9 directory

--- Pertemuan-9/src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import HelloComponent from "./component/HelloComponent";
-
-class App extends Component{
-  render(){
-    return HelloComponent();
-  }
-}
-export default App;
