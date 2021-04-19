7587ea5aa97b66b2668e3046e8f8cbfb1e2c23de
adityapanji <benjol010100@gmail.com>
2021-04-19 12:16:30 +0700

    add pertemuan 10

---
+++ pertemuan-10/src/firebase.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    apiKey: "AIzaSyCjpRJbTHDMnDRIZmxRxaA6IN1FlI8Mj1I",
+    authDomain: "reactfirebase-3f8ce.firebaseapp.com",
+    projectId: "reactfirebase-3f8ce",
+    storageBucket: "reactfirebase-3f8ce.appspot.com",
+    messagingSenderId: "615283615322",
+    appId: "1:615283615322:web:5754758d91f895dc4e5a63",
+    measurementId: "G-BBQ88WK51F"
+
+};
