07873523e4cfe588cc4a57959e2c38f8b489141a
adityapanji <benjol010100@gmail.com>
2021-05-03 10:41:02 +0700

    add pertemuan 12

---
+++ pertemuan-12/install_laravel/resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head><!--Tag head ini gunanya untuk penamaan tab pada browser.-->
+    <title>BIODATA</title>
+</head>
+<body><!-- Tag body ini gunanya untuk mengisikan semua konten yang ada di dalam web-->
+    <h1 align="center">BIODATA</h1><!--Judul Halaman Web-->
+    <hr align="center" width="100%" color="#3a3634" size="2"><!--Untuk Membuat Garis-->
+    <center><!--Tag untuk ke tengah gambarnya-->
+        <img src="https://avatars.githubusercontent.com/u/58581060?v=4" width="300" height="300" alt="image in here"><!--tag img src itu sesuaikan nama file di dalam folder dan file gambar harus di dalam folder yang sama dengan file web kita-->
+    </center>
+    <Card>
+        <Card.Header> 
+            <Card.Body>
+    <h1 align="center" size="36"> Aditya Panji Rachmadan </h1>
+    <h1 align="center" size="36"> 1841720023 </h1>
+    <h1 align="center" size="36"> TI-3E </h1>
+    <h1 align="center" size="36"> Absen 3 </h1>
+    <h1 align="center" size="36"> Teknologi Infromatika  </h1>
+<p align="center"> Hello saya Aditya Panji Rachmadan Mahasiswa Semester 6 yang selalu sambat <br><!--ini untuk link ke halaman selanjutnya --> </p>
+</body>
+</html>
---
+++ pertemuan-12/install_laravel/resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BIODATA</title>
+</head>
+<body>
+    <h1 align="center">BIODATA</h1>
+    <hr align="center" width="100%" color="#3a3634" size="2">
+    <center>
+        <img src="https://avatars.githubusercontent.com/u/58581060?v=4" width="300" height="300" alt="image in here">
+    </center>
+    <h1 align="center" size="36"> Aditya Panji Rachmadan </h1>
+    <h1 align="center" size="36"> 1841720023 </h1>
+    <h1 align="center" size="36"> TI-3E </h1>
+    <h1 align="center" size="36"> Absen 3 </h1>
+    <h1 align="center" size="36"> Teknologi Informasi</h1>
+<p align="center"> Hello saya Aditya Panji Rachmadan Mahasiswa Semester 6 Politeknik Negeri Malang</p>
+</body>
+</html>
